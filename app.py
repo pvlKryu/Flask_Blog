da58022bbfb5728d7348e593281af8bee696f3a9
Pavel <57821178+pvlKryu@users.noreply.github.com>
2022-01-11 08:14:58 +0300

    Бета-версия сервиса
    Договоры и акты проверок приведены к стандартному виду. Добавлена функция печати документов. Появилась возможность составлять отчеты для директора и клиента, выводить на печать. Справка дополнена прейскурантом цен.

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from flask_migrate import Migrate
 from datetime import date, datetime
 
 from werkzeug.utils import redirect  # Импорт функции реального времени
-
-
+from sqlalchemy import func, text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ovo.db'  # Создаем БД
@@ -14,17 +13,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)  # Запускаем БД
 # db.create_all()
 
-migrate = Migrate(app, db) #Миграция для обновления БД (система контроля версий БД)
+# Миграция для обновления БД (система контроля версий БД)
+migrate = Migrate(app, db)
+
 
 class Contract(db.Model):  # создаем класс Договоры
     contract_id = db.Column(db.Integer, primary_key=True)  # создаем поля
-    object_id = db.Column(db.Integer)
-    client_id = db.Column(db.Integer)
-    worker_id = db.Column(db.Integer)
+
+    #object_id = db.Column(db.Integer)
+    #client_id = db.Column(db.Integer)
+    #worker_id = db.Column(db.Integer)
+
     title = db.Column(db.String(100), nullable=False)
+    # ответственное лицо клиента
+    client_person = db.Column(db.String(100), nullable=False)
+    object_adres = db.Column(db.String(100), nullable=False)
+    client_name = db.Column(db.String(100), nullable=False)  # имя клиента
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     cost = db.Column(db.Integer, nullable=False)
+    type_service = db.Column(db.Integer, nullable=False)  # тип услуги
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):  # По запросу будет выдаваться объект + ID
@@ -33,8 +41,8 @@ class Contract(db.Model):  # создаем класс Договоры
 
 class Act(db.Model):  # создаем класс Акты проверок
     act_id = db.Column(db.Integer, primary_key=True)  # создаем поля
-    contract_id = db.Column(db.Integer)
-    worker_id = db.Column(db.Integer)
+    #contract_id = db.Column(db.Integer)
+    #worker_id = db.Column(db.Integer)
     title = db.Column(db.String(100), nullable=False)
     contract_number = db.Column(db.String(100), nullable=False)
     object_adress = db.Column(db.String(100), nullable=False)
@@ -49,7 +57,7 @@ class Act(db.Model):  # создаем класс Акты проверок
 class Client(db.Model):  # создаем класс Клиент
     client_id = db.Column(db.Integer, primary_key=True)  # создаем поля
     name = db.Column(db.String(100), nullable=False)
-    client_person = db.Column(db.String(100), nullable=False)
+    person = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):  # По запросу будет выдаваться объект + ID
         return '<Client %r>' % self.id
@@ -94,7 +102,7 @@ def contracts():
 
     if q:
         contracts = Contract.query.filter(Contract.title.contains(q) | Contract.text.contains(
-            q) | Contract.contract_id.contains(q) | Contract.intro.contains(q)).all()
+            q) | Contract.contract_id.contains(q) | Contract.client_name.contains(q) | Contract.type_service.contains(q) | Contract.object_adres.contains(q) | Contract.intro.contains(q)).all()
     else:
         # Выводим все записи из БД сортируя по дате:
         contracts = Contract.query.order_by(Contract.date.desc()).all()
@@ -109,7 +117,7 @@ def acts():
 
     if q:
         acts = Act.query.filter(Act.title.contains(q) | Act.text.contains(
-            q) | Act.act_id.contains(q) | Act.status.contains(q) | Act.object_adress.contains(q)).all()
+            q) | Act.act_id.contains(q) | Contract.contract_id.contains(q) | Act.status.contains(q) | Act.object_adress.contains(q)).all()
     else:
         # Выводим все записи из БД сортируя по дате:
         acts = Act.query.order_by(Act.date.desc()).all()
@@ -123,7 +131,7 @@ def contract_detail(contract_id):
     # Выводим все записи из БД сортируя по дате:
     contract = Contract.query.get(contract_id)
     # в шаблон передаем список контрактов
-    return render_template("contract_detail.html", contract=contract)
+    return render_template("contract_detail_new.html", contract=contract)
 
 
 @app.route('/contracts/<int:contract_id>/del')
@@ -158,7 +166,7 @@ def act_detail(act_id):
     # Выводим все записи из БД сортируя по дате:
     act = Act.query.get(act_id)
     # в шаблон передаем список контрактов
-    return render_template("act_detail.html", act=act)
+    return render_template("act_detail_new.html", act=act)
 
 
 @app.route('/create_contract', methods=['POST', 'GET'])
@@ -168,11 +176,16 @@ def create_contract():
         intro = request.form['intro']
         text = request.form['text']
         cost = request.form['cost']
+        client_name = request.form['client_name']
+        type_service = request.form['type_service']
+        client_person = request.form['client_person']
+        object_adres = request.form['object_adres']
 
         # Создаем объект, заполняем поля, передавая переменные
-        contract = Contract(title=title, intro=intro, text=text, cost=cost)
+        contract = Contract(title=title, intro=intro, text=text, cost=cost, client_name=client_name,
+                            type_service=type_service, client_person=client_person, object_adres=object_adres)
         try:  # Обрабатываем ошибки
-            if title and intro and text and cost:  # Проверка на заполненность
+            if title and intro and text and cost and client_name and type_service and client_person and object_adres:  # Проверка на заполненность
                 db.session.add(contract)  # Добавляем объект
                 db.session.commit()  # Сохраняем объект
                 # Если успешно - переводим на главную страницy:
@@ -196,6 +209,10 @@ def contracts_update(contract_id):
         contract.intro = request.form['intro']
         contract.text = request.form['text']
         contract.cost = request.form['cost']
+        contract.client_name = request.form['client_name']
+        contract.type_service = request.form['type_service']
+        contract.client_person = request.form['client_person']
+        contract.object_adres = request.form['object_adres']
 
         try:  # Обрабатываем ошибки
             db.session.commit()  # Сохраняем объект
@@ -256,19 +273,63 @@ def acts_update(act_id):
 def signin():
     return render_template("signin.html")
 
+
 @app.route('/law')
 def law():
     return render_template("law.html")
-    
+
+
 @app.route('/rules')
 def rules():
-    return render_template("rules.html")    
+    return render_template("rules.html")
 
-# @app.route('/director_report')
-# def director_report():
 
-#     return render_template("director_report.html")
+@app.route('/price')
+def price():
+    return render_template("price.html")
 
+
+@app.route('/director_report')
+def director_report():
+    contract_amount = Contract.query.count()  # число новых договоров
+    contract_sum = Contract.query.with_entities(
+        func.sum(Contract.cost).label('total')).first().total  # доход по договорам
+    act_amount = Act.query.count()  # число проверок
+    #fake_act_amount = Act.query.filter_by(status='ложный').all().count
+
+    fake_act_amount = db.session.query(Act.status).filter(
+        Act.status == 'Ложный').count()  # число ложных вызовов
+    real_act_amount = act_amount - fake_act_amount  # число реальных вызовов
+    now = datetime.now().date()  # текущая дата
+    return render_template("director_report.html", contract_amount=contract_amount, contract_sum=contract_sum, act_amount=act_amount, fake_act_amount=fake_act_amount, real_act_amount=real_act_amount, now=now)
+
+
+@app.route('/client_report')
+def client_report():
+    q = request.args.get('q')
+
+    if q:
+        act_amount = Act.query.filter(Act.contract_number.contains(q)).count()
+        fake_act_amount = Act.query.filter(Act.contract_number.contains(q)).filter(
+            Act.status == 'Ложный').count()
+        real_act_amount = act_amount - fake_act_amount  # число реальных вызовов
+        contract_sum = Act.query.filter(Contract.contract_id.contains(q)).with_entities(
+            func.sum(Contract.cost).label('total')).first().total
+        now = datetime.now().date()  # текущая дата
+        return render_template("client_report.html", now=now, act_amount=act_amount, fake_act_amount=fake_act_amount, real_act_amount=real_act_amount, contract_sum=contract_sum)
+    else:
+        return render_template("client_report.html")
+    #     # Выводим все записи из БД сортируя по дате:
+    #     acts = Act.query.order_by(Act.date.desc()).all()
+    #     # в шаблон передаем список контрактов
+
+
+# def inject_now():
+#     return {'now': datetime.utcnow()}
+# @app.route('/user/<username>')
+# def show_user(username):
+#     user = User.query.filter_by(username=username).first_or_404()
+#     return render_template('show_user.html', user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
